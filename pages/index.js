401c6729af1cc689e6585ec34e68a874e612a89d
Diegoalesco95 <42728227+Diegoalesco95@users.noreply.github.com>
2020-04-14 18:24:59 -0500

    Intro a getInitialProps/getServerSideProps

--- pages/index.js
+++ pages/index.js
@@ -1,72 +1,68 @@
-export default class extends React.Component {
-  render() {
-    return (
-      <div className='Main'>
-        <div className='logo'>
-          <img src='/platzi-logo.png' alt='Platzi Logo' />
-        </div>
-        <div className='content'>
-          <h1>¡Hola!</h1>
-          <p>Es un placer tenerte como visitante, espero disfrutes del contenido.</p>
-        </div>
-        <div className='footer'>
-          <p>Elaborado con las siguientes tecnologías:</p>
-          <img src='/React.png' alt='React' />
-          <img src='/nextjs.png' alt='Next.js' />
-        </div>
-        <style jsx global>{`
-          body {
-            margin: 0;
-            padding: 0;
-            background: #2d3436;
-            font-family: Arial;
-            text-align: center;
-          }
-        `}</style>
-        <style jsx>
-          {`
-            .Main {
-              height: 100vh;
-              display: flex;
-              flex-direction: column;
-              justify-content: center;
-              align-content: center;
-              max-width: 80%;
-              margin: 0 auto;
-            }
-            .content {
-              max-height: 200px;
-              margin: 20px 0;
-              padding: 0 20px;
-            }
-            .logo img {
-              max-height: 50%;
-              max-width: 100px;
-              display: block;
-              margin: 20px auto;
-            }
-            h1 {
-              color: #dfe6e9;
-            }
-            p {
-              color: #dfe6e9;
-              font-size: 1.2em;
-            }
-            .footer {
-              margin: 20px auto;
-              padding: 0 20px;
-              display: flex;
-              flex-direction: column;
-              justify-content: center;
-            }
-            .footer img {
-              max-width: 100px;
-              max-height: 50%;
-              margin: 10px auto;
-            }
-          `}
-        </style>
+import fetch from 'isomorphic-fetch'
+
+function App({ channels }) {
+  return (
+    <>
+      <header>The Podcasts</header>
+
+      <div className='channels'>
+        {channels.map((channel) => (
+          <div className='channel'>
+            <img src={channel.urls.logo_image.original} alt='images podcasts' />
+            <h2>{channel.title}</h2>
+          </div>
+        ))}
       </div>
-    )
-  }
+
+      <style jsx global>{`
+        body {
+          margin: 0;
+          padding: 0;
+          background: #2d3436;
+          font-family: system-ui;
+        }
+      `}</style>
+      <style jsx>{`
+        header {
+          color: #fff;
+          background: #8756ca;
+          padding: 15px;
+          text-align: center;
+        }
+        .channels {
+          display: grid;
+          grid-gap: 15px;
+          padding: 15px;
+          grid-template-columns: repeat(auto-fill, minmax(160px, 1fr));
+        }
+        a.channel {
+          display: block;
+          margin-bottom: 0.5em;
+          color: #333;
+          text-decoration: none;
+        }
+        .channel img {
+          border-radius: 3px;
+          box-shadow: 0px 2px 6px rgba(0, 0, 0, 0.15);
+          width: 100%;
+        }
+        h2 {
+          padding: 5px;
+          font-size: 0.9em;
+          font-weight: 600;
+          margin: 0;
+          text-align: center;
+          color: #dfe6e9;
+        }
+      `}</style>
+    </>
+  )
 }
+
+export async function getServerSideProps() {
+  let req = await fetch('https://api.audioboom.com/channels/recommended')
+  let { body: channels } = await req.json()
+  return { props: { channels } }
+}
+
+export default App
