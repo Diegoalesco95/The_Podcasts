d6bce83529b6bef6e0d9fefee600b10229b93840
Diegoalesco95 <42728227+Diegoalesco95@users.noreply.github.com>
2020-04-15 11:45:42 -0500

    Perfomance de Get Server Side Props

--- pages/channel.js
+++ pages/channel.js
@@ -1,18 +1,37 @@
 import fetch from 'isomorphic-fetch'
+import Link from 'next/link'
 
 function Channel({ channel, audioClips, series }) {
   return (
     <>
       <header>The Podcasts</header>
+      <div className='banner' style={{ backgroundImage: `url(${channel.urls.banner_image.original})` }} />
+
       <h1>{channel.title}</h1>
-      <h2>Series</h2>
-      {series.map((serie) => (
-        <div>{serie.title}</div>
-      ))}
-      <h2>Ultimos Podcast</h2>
+
+      {series.length > 0 && (
+        <div>
+          <h2>Series</h2>
+          <div className='channels'>
+            {series.map((serie) => (
+              <Link href={`/channel?id=${serie.id}`}>
+                <a className='channel'>
+                  <img src={serie.urls.logo_image.original} alt='' />
+                  <h3>{serie.title}</h3>
+                </a>
+              </Link>
+            ))}
+          </div>
+        </div>
+      )}
+
+      <h2>Últimos Podcasts</h2>
       {audioClips.map((clip) => (
-        <div>{clip.title}</div>
+        <div className='podcast' key={clip.id}>
+          {clip.title}
+        </div>
       ))}
+
       <style jsx global>{`
         body {
           margin: 0;
@@ -27,6 +46,13 @@ function Channel({ channel, audioClips, series }) {
           background: #8756ca;
           padding: 15px;
           text-align: center;
+        }
+        .banner {
+          width: 100%;
+          padding-bottom: 25%;
+          background-position: 50% 50%;
+          background-size: cover;
+          background-color: #aaa;
         }
         .channels {
           display: grid;
@@ -45,7 +71,21 @@ function Channel({ channel, audioClips, series }) {
           box-shadow: 0px 2px 6px rgba(0, 0, 0, 0.15);
           width: 100%;
         }
+        h1 {
+          font-weight: 600;
+          padding: 15px;
+          color: #dfe6e9;
+        }
         h2 {
+          padding: 5px;
+          font-size: 1.2em;
+          font-weight: 600;
+          text-transform: uppercase;
+          margin: 0;
+          text-align: center;
+          color: #dfe6e9;
+        }
+        h3 {
           padding: 5px;
           font-size: 0.9em;
           font-weight: 600;
@@ -53,10 +93,24 @@ function Channel({ channel, audioClips, series }) {
           text-align: center;
           color: #dfe6e9;
         }
-        h1 {
-          font-weight: 600;
-          padding: 15px;
+        .podcast {
+          display: block;
+          text-decoration: none;
           color: #dfe6e9;
+          padding: 15px;
+          border-bottom: 1px solid rgba(255, 255, 255, 0.2);
+          cursor: pointer;
+        }
+        .podcast:hover {
+          color: #8756ca;
+        }
+        .podcast h3 {
+          margin: 0;
+        }
+        .podcast .meta {
+          color: #666;
+          margin-top: 0.5em;
+          font-size: 0.8em;
         }
       `}</style>
     </>
@@ -65,15 +119,19 @@ function Channel({ channel, audioClips, series }) {
 
 export async function getServerSideProps({ query }) {
   let idChannel = query.id
-  let reqChannel = await fetch(`https://api.audioboom.com/channels/${idChannel}`)
+
+  let [reqChannel, reqSeries, reqAudios] = await Promise.all([
+    fetch(`https://api.audioboom.com/channels/${idChannel}`),
+    fetch(`https://api.audioboom.com/channels/${idChannel}/child_channels`),
+    fetch(`https://api.audioboom.com/channels/${idChannel}/audio_clips`)
+  ])
+
   let dataChannel = await reqChannel.json()
   let channel = dataChannel.body.channel
 
-  let reqSeries = await fetch(`https://api.audioboom.com/channels/${idChannel}/child_channels`)
   let dataSeries = await reqSeries.json()
   let series = dataSeries.body.channels
 
-  let reqAudios = await fetch(`https://api.audioboom.com/channels/${idChannel}/audio_clips`)
   let dataAudios = await reqAudios.json()
   let audioClips = dataAudios.body.audio_clips
 
